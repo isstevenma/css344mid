3c8b935b0b68f6ee31e5288b8735d40e29b02b9e
issteven <t1z1b@ugrad.cs.ubc.ca>
2021-11-25 10:48:52 -0800

    Added links, fixed grades, fixed typos

--- pages/degree.js
+++ pages/degree.js
@@ -23,12 +23,12 @@ export default function Degree() {
         text="Research Component"
         completed="0% | 0/3"
         cip="4"
-        icon={<CheckIcon/>}
+        icon={<CancelIcon/>}
         key="r2"
     />,
     //add link
     <Collapsible
-        contents={[<Course text="Link to suggestions" key="1"/>]}
+        contents={[<Course text={<a href="https://www.calendar.ubc.ca/Vancouver/index.cfm?tree=12,197,282,55#1220">Link to Suggestions</a>} key="1"/>]}
         text="Language"
         completed="0% | 0/3"
         cip="0"
@@ -36,8 +36,8 @@ export default function Degree() {
         key="r3"
     />,
     <Collapsible
-    contents={[<Course text="ENGL 110" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-               <Course text="ENGL 242" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+    contents={[<Course text="ENGL 110" grade="Grade: 80" credits="Credits: 3" key="1"/>,
+               <Course text="ENGL 242" grade="Grade: 70" credits="Credits: 3" key="1"/>]}
     text="Literature"
     completed="100% | 6/6"
     cip="0"
@@ -45,8 +45,8 @@ export default function Degree() {
     key="r4"
 />,
     <Collapsible
-    contents={[<Course text="Math 104" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-               <Course text="Math 105" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+    contents={[<Course text="Math 104" grade="Grade: 70" credits="Credits: 3" key="1"/>,
+               <Course text="Math 105" grade="Grade: 60" credits="Credits: 3" key="1"/>]}
     text="First Year Math"
     completed="100% | 6/6"
     cip="0"
@@ -54,7 +54,7 @@ export default function Degree() {
     key="r5"
 />,
 <Collapsible
-    contents={[<Course text="LATN 101" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+    contents={[<Course text="LATN 101" grade="Grade: 85" credits="Credits: 3" key="1"/>]}
     text="Elective"
     completed="100% | 3/3"
     cip="0"
@@ -71,8 +71,8 @@ export default function Degree() {
     let bee = [
         <Collapsible
         contents={[<Collapsible
-            contents={[<Course text="CPSC 110" grade="Grade: 100" credits="Credits: 4" key="1"/>,
-                       <Course text="CPSC 121" grade="Grade: 100" credits="Credits: 4" key="1"/>]}
+            contents={[<Course text="CPSC 110" grade="Grade: 90" credits="Credits: 4" key="1"/>,
+                       <Course text="CPSC 121" grade="Grade: 85" credits="Credits: 4" key="1"/>]}
             text="First Year CPSC"
             completed="100% | 8/7"
             cip="0"
@@ -80,9 +80,9 @@ export default function Degree() {
             key="r6"
         />,
         <Collapsible
-            contents={[<Course text="CPSC 210" grade="Grade: 100" credits="Credits: 4" key="1"/>,
-                       <Course text="CPSC 213" grade="Grade: 100" credits="Credits: 4" key="1"/>,
-                       <Course text="CPSC 221" grade="Grade: 100" credits="Credits: 4" key="1"/>]}
+            contents={[<Course text="CPSC 210" grade="Grade: 80" credits="Credits: 4" key="1"/>,
+                       <Course text="CPSC 213" grade="Grade: 85" credits="Credits: 4" key="1"/>,
+                       <Course text="CPSC 221" grade="Grade: 80" credits="Credits: 4" key="1"/>]}
             text="Second Year CPSC"
             completed="100% | 12/12"
             cip="0"
@@ -90,8 +90,8 @@ export default function Degree() {
             key="r7"
         />,
         <Collapsible
-            contents={[<Course text="MATH 200" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-                       <Course text="MATH 221" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+            contents={[<Course text="MATH 200" grade="Grade: 90" credits="Credits: 3" key="1"/>,
+                       <Course text="MATH 221" grade="Grade: 60" credits="Credits: 3" key="1"/>]}
             text="Second Year Math"
             completed="100% | 6/6"
             cip="0"
@@ -99,7 +99,7 @@ export default function Degree() {
             key="r8"
         />,
         <Collapsible
-            contents={[<Course text="CPSC 310" grade="Grade: 100" credits="Credits: 4" key="1"/>,
+            contents={[<Course text="CPSC 310" grade="Grade: 70" credits="Credits: 4" key="1"/>,
                        <Course text="CPSC 313" grade="Grade: CIP" credits="Credits: 3" key="1"/>,
                        <Course text="CPSC 320" grade="Grade: CIP" credits="Credits: 3" key="1"/>]}
             text="Third Year Requirements"
@@ -109,7 +109,8 @@ export default function Degree() {
             key="r9"
         />,
         <Collapsible
-            contents={[<Course text="CPSC 319" grade="Grade: 100" credits="Credits: 4" key="1"/>]}
+            contents={[<Course text="CPSC 319" grade="Grade: 80" credits="Credits: 4" key="1"/>,
+                       <Course text={<a href="https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-department&dept=CPSC">Link to suggestions</a>} key="1"/>]}
             text="300 or above Courses"
             completed="33% | 4/12"
             cip="0"
@@ -117,7 +118,7 @@ export default function Degree() {
             key="r10"
         />,
         <Collapsible
-            contents={[<Course text="Link to suggestions" key="1"/>]}
+            contents={[<Course text={<a href="https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-department&dept=CPSC">Link to suggestions</a>} key="1"/>]}
             text="400 or above Courses"
             completed="0%"
             cip="0/9"
@@ -135,8 +136,9 @@ export default function Degree() {
     let electives = [
         <Collapsible
         contents = {[<Collapsible
-            contents={[<Course text="ENGL 227" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-                <Course text="CRWR 200" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+            contents={[<Course text="ENGL 227" grade="Grade: 80" credits="Credits: 3" key="1"/>,
+                <Course text="CRWR 200" grade="Grade: 60" credits="Credits: 3" key="1"/>,
+                <Course text={<a href="https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea">Link to suggestions</a>}/>]}
             text="Electives Outside of CPSC"
             completed="15% | 6/39"
             cip="0"
@@ -144,9 +146,10 @@ export default function Degree() {
             key="r12"
         />,
         <Collapsible
-            contents={[<Course text="ENGL 112" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-                <Course text="CRWR 203" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-                <Course text="CRWR 213" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+            contents={[<Course text="ENGL 112" grade="Grade: 80" credits="Credits: 3" key="1"/>,
+                <Course text="CRWR 203" grade="Grade: 90" credits="Credits: 3" key="1"/>,
+                <Course text="CRWR 213" grade="Grade: 70" credits="Credits: 3" key="1"/>,
+                <Course text={<a href="https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea">Link to suggestions</a>}/>]}
             text="Electives (Any)"
             completed="100% | 9/7"
             cip="0"
@@ -163,10 +166,12 @@ export default function Degree() {
 
     let upperLevel = [
         <Collapsible
-            contents={[<Course text="CPSC 310" grade="Grade: 100" credits="Credits: 4" key="1"/>,
-                <Course text="CPSC 313" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-                <Course text="CPSC 319" grade="Grade: 100" credits="Credits: 4" key="4"/>,
-                <Course text="CPSC 320" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+            contents={[<Course text="CPSC 310" grade="Grade: 90" credits="Credits: 4" key="1"/>,
+                <Course text="CPSC 313" grade="Grade: 60" credits="Credits: 3" key="1"/>,
+                <Course text="CPSC 319" grade="Grade: 85" credits="Credits: 4" key="4"/>,
+                <Course text="CPSC 320" grade="Grade: 90" credits="Credits: 3" key="1"/>,
+                <Course text={<a href="https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-department&dept=CPSC">Link to suggestions</a>} key="1"/>
+            ]}
             text="Upper-Level Courses"
             completed="21% | 10/48"
             cip="0"
--- seed/mediaStudies.js
+++ seed/mediaStudies.js
@@ -8,22 +8,22 @@ import CancelIcon from '@mui/icons-material/Cancel';
 let coureCourses = [
     <Collapsible
     contents={[
-        <Course text="VISA110" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-        <Course text="VISA210" grade="Grade: 100" credits="Credits: 3" key="2"/>,
-        <Course text="VISA241" grade="Grade: 100" credits="Credits: 3" key="3"/>,
-        <Course text="JRNL100" grade="Grade: 100" credits="Credits: 3" key="4"/>,
-        <Course text="INFO250" grade="Grade: 100" credits="Credits: 3" key="5"/>,
-        <Course text="FIST100" grade="Grade: 100" credits="Credits: 3" key="6"/>,
-        <Course text="FIST240" grade="Grade: 100" credits="Credits: 3" key="7"/>,
-        <Course text="FIPR133" grade="Grade: 100" credits="Credits: 3" key="8"/>,
-        <Course text="ENGL232" grade="Grade: 100" credits="Credits: 3" key="9"/>,
-        <Course text="CRWR213" grade="Grade: 100" credits="Credits: 3" key="10"/>,
-        <Course text="CPSC110" grade="Grade: 100" credits="Credits: 3" key="11"/>,
-        <Course text="ASTU100" grade="Grade: 100" credits="Credits: 3" key="12"/>,
-        <Course text="ARTH380" grade="Grade: CIP" credits="Credits: 3" key="13"/>,
-        <Course text="CRWR312" grade="Grade: CIP" credits="Credits: 3" key="14"/>,
-        <Course text="ENGL332" grade="Grade: CIP" credits="Credits: 3" key="15"/>,
-        <Course text="Link to other required courses"/>
+        <Course text="VISA110" grade="Grade: 85" credits="Credits: 3" key="1"/>,
+        <Course text="VISA210" grade="Grade: 90" credits="Credits: 3" key="2"/>,
+        <Course text="VISA241" grade="Grade: 75" credits="Credits: 3" key="3"/>,
+        <Course text="JRNL100" grade="Grade: 60" credits="Credits: 3" key="4"/>,
+        <Course text="INFO250" grade="Grade: 83" credits="Credits: 3" key="5"/>,
+        <Course text="FIST100" grade="Grade: 92" credits="Credits: 3" key="6"/>,
+        <Course text="FIST240" grade="Grade: 85" credits="Credits: 3" key="7"/>,
+        <Course text="FIPR133" grade="Grade: 82" credits="Credits: 3" key="8"/>,
+        <Course text="ENGL232" grade="Grade: 72" credits="Credits: 3" key="9"/>,
+        <Course text="CRWR213" grade="Grade: 79" credits="Credits: 3" key="10"/>,
+        <Course text="CPSC110" grade="Grade: 89" credits="Credits: 3" key="11"/>,
+        <Course text="ASTU100" grade="Grade: 84" credits="Credits: 3" key="12"/>,
+        <Course text="ARTH380" grade="Grade: 81" credits="Credits: 3" key="13"/>,
+        <Course text="CRWR312" grade="Grade: 75" credits="Credits: 3" key="14"/>,
+        <Course text="ENGL332" grade="Grade: 63" credits="Credits: 3" key="15"/>,
+        <Course text={<a href="https://www.calendar.ubc.ca/Vancouver/index.cfm?tree=12,197,955,1523">Link to other required courses</a>}/>
     ]}
     text="Core Courses"
     completed="68% | 36/66"
@@ -37,10 +37,10 @@ let courePlusCourses = [
     <Collapsible
     contents={[
         <Course text="CRWR206" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-        <Course text="CPSC210" grade="Grade: 100" credits="Credits: 3" key="2"/>,
-        <Course text="ACAM250" grade="Grade: 100" credits="Credits: 3" key="3"/>,
+        <Course text="CPSC210" grade="Grade: 92" credits="Credits: 3" key="2"/>,
+        <Course text="ACAM250" grade="Grade: 82" credits="Credits: 3" key="3"/>,
         <Course text="ENGL201" grade="Grade: CIP" credits="Credits: 3" key="4"/>,
-        <Course text="Link to other qualifiable courses"/>
+        <Course text={<a href="https://www.calendar.ubc.ca/Vancouver/index.cfm?tree=12,197,955,1523">Link to other qualifiable courses"</a>}/>
     ]}
     text="Core Plus Courses"
     completed="75% | 9/12"
@@ -52,11 +52,12 @@ let courePlusCourses = [
 
 let electives = [
     <Collapsible
-        contents={[<Course text="ENGL 112" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-        <Course text="ENGL 227" grade="Grade: 100" credits="Credits: 3" key="2"/>,
-        <Course text="CRWR 200" grade="Grade: 100" credits="Credits: 3" key="3"/>,
-            <Course text="CRWR 203" grade="Grade: 100" credits="Credits: 3" key="4"/>,
-            <Course text="CRWR 213" grade="Grade: 100" credits="Credits: 3" key="5"/>
+        contents={[<Course text="ENGL 112" grade="Grade: 83" credits="Credits: 3" key="1"/>,
+        <Course text="ENGL 227" grade="Grade: 85" credits="Credits: 3" key="2"/>,
+        <Course text="CRWR 200" grade="Grade: 81" credits="Credits: 3" key="3"/>,
+            <Course text="CRWR 203" grade="Grade: 70" credits="Credits: 3" key="4"/>,
+            <Course text="CRWR 213" grade="Grade: 75" credits="Credits: 3" key="5"/>,
+            <Course text={<a href="https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea">Link to suggestions</a>}/>
             ]}
         text="Electives (Any)"
         completed="33% | 15/45"
@@ -68,10 +69,11 @@ let electives = [
 
 let upperLevel = [
     <Collapsible
-        contents={[<Course text="CPSC 310" grade="Grade: 100" credits="Credits: 4" key="1"/>,
-            <Course text="CPSC 313" grade="Grade: 100" credits="Credits: 3" key="1"/>,
-            <Course text="CPSC 319" grade="Grade: 100" credits="Credits: 4" key="4"/>,
-            <Course text="CPSC 320" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+        contents={[
+        <Course text="CPSC 310" grade="Grade: 100" credits="Credits: 3" key="1"/>,
+        <Course text="ARTH380" grade="Grade: CIP" credits="Credits: 3" key="13"/>,
+        <Course text="CRWR312" grade="Grade: CIP" credits="Credits: 3" key="14"/>,
+        <Course text="ENGL332" grade="Grade: CIP" credits="Credits: 3" key="15"/>]}
         text="Upper-level Courses"
         completed="21% | 10/48"
         cip="0"
@@ -82,7 +84,7 @@ let upperLevel = [
 
 let arts = [<Collapsible
     contents={[<Collapsible
-        contents={[<Course text="ENGL100" grade="Grade: 100" credits="Credits: 3" key="1"/>]}
+        contents={[<Course text="ENGL100" grade="Grade: 80" credits="Credits: 3" key="1"/>]}
         text="Writing Component"
         completed="100% | 3/3"
         icon={<CheckIcon/>}
